a7b4a9868c64380707b67f05d3fdaf8fab1ee189
bd4everyone <p1romita@yahoo.co.in>
2025-08-12 13:50:25 -0400

    Add files via upload

---
+++ update_projects.py
@@ -0,0 +1,180 @@
+import requests
+import pandas as pd
+#from IPython.display import display
+import os
+import json
+from html import escape
+#import tabulate
+#import itables
+# ==============================================================================
+# --- Configuration ---
+# ==============================================================================
+# Set the search mode. Options are: 'TOPICS', 'ORG', 'USER'
+SEARCH_MODE = 'TOPICS'
+
+# --- Search Terms ---
+# For 'TOPICS' mode, provide a list of topics.
+#searches for repos with both topics -- only three repositories with both topics
+SEARCH_TOPICS = ["u24ca289073","nci-itcr"]
+#49 projects with topic nci-itcr
+SEARCH_TOPICS_1 = ["nci-itcr"]
+#49/50 projects with topic u24
+SEARCH_TOPICS_2 = ["u24ca289073"]
+
+# For 'ORG' mode, provide the organization's name.
+SEARCH_ORG = "exampleorg"
+
+# For 'USER' mode, provide the user's GitHub handle.
+SEARCH_USER = "exampleuser"
+
+#--------------------Hardcode Biocondcutor url mapping -------------------------------------
+#hardcode u24,nci-itcr bioconductor packages
+#check for nci-itcr packages in biocondcutor - step through repo page
+HARDCODED_BIOC_URLS = {"MultiAssayExperiment" : "https://www.bioconductor.org/packages/release/bioc/html/MultiAssayExperiment.html","BiocParallel" : "https://www.bioconductor.org/packages/release/bioc/html/BiocParallel.html","SpatialExperiment" : "https://www.bioconductor.org/packages/release/bioc/html/SpatialExperiment.html", "curatedTCGAData" : "https://www.bioconductor.org/packages/release/data/experiment/html/curatedTCGAData.html","cBioPortalData" : "https://www.bioconductor.org/packages/release/bioc/html/cBioPortalData.html","TCGAutils" : "https://www.bioconductor.org/packages/release/bioc/html/TCGAutils.html","DelayedArray" : "https://bioconductor.org/packages/release/bioc/html/DelayedArray.html","BiocPkgTools" : "https://www.bioconductor.org/packages/release/bioc/html/BiocPkgTools.html","AnnotationHub" : "https://bioconductor.org/packages/devel/bioc/html/AnnotationHub.html","SingleCellMultiModal" : "https://www.bioconductor.org/packages/release/data/experiment/html/SingleCellMultiModal.html","GSEABenchmarkeR" : "https://bioconductor.org/packages/release/bioc/html/GSEABenchmarkeR.html","HDF5Array" : "https://www.bioconductor.org/packages/release/bioc/html/HDF5Array.html","curatedOvarianData" : "https://www.bioconductor.org/packages/release/data/experiment/html/curatedOvarianData.html","SparseArray" : "https://bioconductor.org/packages/devel/bioc/html/SparseArray.html","CNVRanger" : "https://bioconductor.org/packages/devel/bioc/html/CNVRanger.html","S4Arrays" : "https://www.bioconductor.org/packages/release/bioc/html/S4Arrays.html","h5mread" : "https://bioconductor.org/packages/release/bioc/html/h5mread.html","VisiumIO" : "https://www.bioconductor.org/packages/release/bioc/html/VisiumIO.html","TENxIO" : "https://www.bioconductor.org/packages/release/bioc/html/TENxIO.html"}
+# ==============================================================================
+
+# --- Build the search query based on the selected mode ---
+query_parts = []
+title = ""
+if SEARCH_MODE == 'TOPICS':
+    url_1='https://api.github.com/search/repositories?q=topic:u24ca289073&per_page=100'
+    url_2='https://api.github.com/search/repositories?q=topic:nci-itcr&per_page=100'
+    title = f"Projects with Topics: {', '.join(SEARCH_TOPICS)}"
+elif SEARCH_MODE == 'ORG':
+    query_parts.append(f"org:{SEARCH_ORG}")
+    title = f"Projects in Organization: {SEARCH_ORG}"
+elif SEARCH_MODE == 'USER':
+    query_parts.append(f"user:{SEARCH_USER}")
+    title = f"Projects for User: {SEARCH_USER}"
+
+#url equals github url
+#search_query = "("+".join(query_parts_1,))"
+#search_query = "+".join(query_parts)
+
+# --- Get data from GitHub API ---
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+headers = {"Authorization": f"token {GITHUB_TOKEN}"} if GITHUB_TOKEN else None
+
+if not GITHUB_TOKEN:
+    print("Warning: GITHUB_TOKEN environment variable not set.")
+
+try:
+    response_1 = requests.get(url_1, headers=headers)
+    response_1.raise_for_status()
+    data_1 = response_1.json()
+    response_2 = requests.get(url_2, headers=headers)
+    response_2.raise_for_status()
+    data_2 = response_2.json()
+    repo_list_1 = data_1['items']
+    repo_list_2 = data_2['items']
+    combined_repos = {repo['id']: repo for repo in repo_list_1}
+    combined_repos.update({repo['id']: repo for repo in repo_list_2})
+    # Convert the dictionary back to a list of repositories
+    final_repo_list = list(combined_repos.values())
+    projects = []
+    repo_list_for_json = []
+    for item in final_repo_list:
+    #
+        # Sanitize description to prevent HTML issues
+        description = escape(item["description"]) if item["description"] else ""
+        repo_name = item["name"]
+        bioconductor_link = None 
+        if repo_name in HARDCODED_BIOC_URLS:
+            bioconductor_url = HARDCODED_BIOC_URLS[repo_name]
+            bioconductor_link = f'<a href="{bioconductor_url}" target="_blank"> </a>'
+        else:
+            # Attempt to construct and validate a Bioconductor URL based on naming convention
+            potential_bioc_package_name = repo_name
+            bioc_base_url = "https://bioconductor.org/packages/release/bioc/html/"   
+            temp_bioc_url = f"{bioc_base_url}{potential_bioc_package_name}.html"
+            try:
+                bioc_response = requests.head(temp_bioc_url, timeout=5)
+                if bioc_response.status_code == 200:
+                    bioconductor_url = temp_bioc_url
+                    bioconductor_link = f'<a href="{bioconductor_url}" target="_blank"> </a>'
+            except requests.exceptions.RequestException as e:
+               pass 
+    #https://bioconductor.org/packages/release/bioc/html/bugsigdbr.html
+        repo_column_html = ""
+        #include bioconductor url if available and github icon
+        if bioconductor_link:
+            repo_column_html += f'<a href="{bioconductor_url}" target="_blank" style="color: red;">{repo_name}</a>'
+        else:
+            repo_column_html = f'{repo_name}'
+
+        repo_column_html += (
+        f'<a href="{item["html_url"]}" target="_blank">'
+        f'<img src="githubicon.svg" style="height: 0.9em; vertical-align: middle; margin-left: 10px; margin-right: 10px">'
+        f'</a>')
+        if bioconductor_link:
+           repo_column_html += (
+            f'<a href="{bioconductor_url}" target="_blank"> </a>')
+
+        projects.append({
+            # Create a clickable link for the repository name
+            "Repository": repo_column_html,
+            "Owner": item["owner"]["login"],
+            "Description": description,
+            "Stars": item["stargazers_count"],
+            # Use 'pushed_at' for the last commit date, not 'updated_at'
+            "Last Updated": item["pushed_at"].split("T")[0],
+        })
+        repo_list_for_json.append(item["full_name"])
+
+    if projects:
+        # --- Create HTML Page ---
+        df = pd.DataFrame(projects)
+        html_table = df.to_html(escape=False, index=False)
+        #print("xxxxx",df,"\n")
+        # Write the content to an quarto markdown file
+        with open("projects.qmd", "w", encoding="utf-8") as f:
+            f.write("---\n")
+            f.write('title: "U24CancerProjects with Topics: u24ca289073 or nci-itcr" \n')
+            f.write('subtitle: "A list of repositories discovered on GitHub. The table is sortable by clicking on headers and searchable via the search box."\n')
+            f.write("format:\n")
+            f.write("  html:\n")
+            #f.write("    css: style.css\n")
+            f.write("    page-layout: full\n")
+            f.write("    df-print: paged\n")
+            f.write("---\n\n")
+            f.write('<style>')
+            f.write('.dataframe {\n')
+            f.write('width: 100%;\n')
+            f.write('border-collapse: collapse;\n')
+            f.write('margin-top: 20px;\n')
+            f.write('}\n') 
+            f.write('.dataframe th, .dataframe td {\n')
+            f.write('border: 1px solid #ddd;\n')
+            f.write('padding: 8px;\n')
+            f.write('text-align: left;\n')
+            f.write('}\n') 
+            f.write('.dataframe th {\n')
+            f.write('background-color: #f2f2f2;\n')
+            f.write('cursor: pointer;\n')
+            f.write('}\n')
+            f.write('</style>\n\n')
+            f.write('<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css"> \n\n')
+            f.write(html_table)
+            f.write('\n\n')
+            f.write('<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.6.0.min.js"></script> \n')
+            f.write('<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script> \n')
+            f.write('<script>\n')
+            f.write('$(document).ready( function () { \n')
+            f.write("$('.dataframe').DataTable();\n")
+            f.write('} );\n')
+            f.write('</script> \n')  
+        print(f"Successfully created projects.qmd with {len(projects)} projects.")
+
+        # Write the JSON file for the commit log script (no changes here)
+        with open("repositories.json", "w") as f:
+            json.dump(repo_list_for_json, f)
+        print(f"Successfully created repositories.json.")
+
+    else:
+        print(f"No projects found for the specified criteria.")
+
+except requests.exceptions.HTTPError as e:
+    print(f"Failed to retrieve data: {e.response.status_code}")
+    print(f"Response: {e.response.text}")
+except Exception as e:
+    print(f"An error occurred: {e}")
